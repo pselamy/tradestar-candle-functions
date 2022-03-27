5e1c5b17e425c6b6ddbe69ff07bb9597ff5bee21
Patrick Selamy <pselamy@gmail.com>
2022-03-27 16:07:52 -0500

    Create GetCandles skeleton.

--- src/main/java/com/verlumen/tradestar/functions/candles/fission/GetCandles.java
+++ src/main/java/com/verlumen/tradestar/functions/candles/fission/GetCandles.java
@@ -1,3 +1,14 @@
 package com.verlumen.tradestar.functions.candles.fission;
 
-public class GetCandles {}
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+
+import io.fission.Function;
+import io.fission.Context;
+
+public class GetCandles implements Function<Object, Object> {
+    @Override
+    public ResponseEntity<?> call(RequestEntity req, Context context) {
+        return ResponseEntity.ok("Hello World!");
+    }
+}
